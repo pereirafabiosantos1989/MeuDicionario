a88c78c726286fe6b3b30762e3155cacea210efc
Fabio <pereirafabiosantos@outlook.com>
2020-02-13 20:58:22 -0300

    Criação das demais ViewModel

---
+++ MeuDicionario/ViewModel/CadastrarTermoViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MeuDicionario.ViewModel
+{
+    public class CadastrarTermoViewModel : BasePropertyChange
+    {
+
+    }
+}
