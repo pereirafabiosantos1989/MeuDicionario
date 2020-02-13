5f4d68ac25af79b9240f69aa65ba60224219440e
Fabio <pereirafabiosantos@outlook.com>
2020-02-13 20:57:54 -0300

    Implementação da classe que cuidará do INotifyPropertyChanged

---
+++ MeuDicionario/ViewModel/BasePropertyChange.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace MeuDicionario.ViewModel
+{
+    public class BasePropertyChange : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName] string property = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property));
+        }
+    }
+}
