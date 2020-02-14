f64e6fe6ce319af878f99bbc617b4ccf15c7e4a9
Fabio <pereirafabiosantos@outlook.com>
2020-02-13 21:01:58 -0300

    Implementação do Model Idioma

---
+++ MeuDicionario/Modelo/Idioma.cs
@@ -0,0 +1,13 @@
+﻿using SQLite;
+
+namespace MeuDicionario.Modelo
+{
+    public class Idioma
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+
+        public string Nome { get; set; }
+
+    }
+}
